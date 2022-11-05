3733ec8bddaf21e824b86082c54f2cf06bb96ba9
AJISAKA SIDDIQ <ajisakasiddiq@gmail.com>
2022-11-05 09:32:19 +0700

    Update console.js

--- console.js
+++ console.js
@@ -1,5 +1,6 @@
 console.log("hello world")
-console.log("hello world")//2nd
+console.log("hello world") //2nd
 
 
-console.log("hello world")//2nd
+console.log("hello world") //2nd
+console.log("hello world") //2nd
